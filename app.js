1c1b0eacddfac4f76d14381a76f3bf8b474e52f4
Siena Santos <aneis_siena18@yahoo.com>
2019-04-05 18:07:48 +0800

    login and register

--- app.js
+++ app.js
@@ -1,53 +1,54 @@
-var express = require('express');
-var compression = require('compression');
-var path = require('path');
-var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var mongoose = require('mongoose');
+var express = require("express");
+var compression = require("compression");
+var path = require("path");
+var favicon = require("serve-favicon");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var mongoose = require("mongoose");
 // var helmet = require('helmet');
-var cors = require('cors')
-var logger = require('morgan');
-var MongoStore = require('connect-mongo')(session);
+var cors = require("cors");
+var logger = require("morgan");
+var MongoStore = require("connect-mongo")(session);
 
 // var json2csv = require('express-json2csv');
 // var busboy = require('connect-busboy');
 // var busboyBodyParser = require('busboy-body-parser');
 
 var dburi =
-    process.env.MONGODB_URI ||
-    process.env.MONGOHQ_URL ||
-    'mongodb://localhost/rfc360-admin';
+  process.env.MONGODB_URI ||
+  process.env.MONGOHQ_URL ||
+  "mongodb://localhost/hackathon";
 //
-mongoose.connect(dburi, function (err, res) {
+mongoose.connect(
+  dburi,
+  function(err, res) {
     if (err) {
-        console.log ('ERROR connecting to: ' + dburi + '. ' + err);
+      console.log("ERROR connecting to: " + dburi + ". " + err);
     } else {
-        console.log ('Succeeded connected to: ' + dburi);
+      console.log("Succeeded connected to: " + dburi);
     }
-});
+  }
+);
 
 var sess = {
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: false,
-    store: new MongoStore({mongooseConnection: mongoose.connection}),
-    cookie: {maxAge : 43200000} //from 8hrs ,change to 12hrs
-}
-
+  secret: "keyboard cat",
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  cookie: { maxAge: 43200000 } //from 8hrs ,change to 12hrs
+};
 
 //routes declaration
-var routes = require('./routes/index');
-
+var routes = require("./routes/index");
 
 var app = express();
-app.set('trust proxy', 1) // trust first proxy
+app.set("trust proxy", 1); // trust first proxy
 
-app.use(cors())
+app.use(cors());
 
 app.use(compression());
-app.use(logger('dev'));
+app.use(logger("dev"));
 // app.use(pino)
 // app.use(busboy());
 // app.use(busboyBodyParser());
@@ -55,11 +56,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cookieParser());
 // app.use(json2csv)
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session(sess));
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(session(sess));
+
+app.use(session({ secret: "keyboard cat", cookie: { maxAge: 60000 } }));
 
 //routes
-app.use('/', routes);
+app.use("/", routes);
 
 // if (app.get('env') === 'production') {
 //
@@ -93,40 +96,35 @@ app.use('/', routes);
 //
 // }
 
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/img', 'cs-icon.png')));
-
+app.use(favicon(path.join(__dirname, "public/img", "cs-icon.png")));
 
 // webpack middleware
 // only do this in dev, in prod -- serve actual bundled files
 
 // webpack imports
-  var webpackMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware')
-  var webpack = require('webpack');
-  var config = require('./webpack.config.js');
-  var compiler = webpack(config)
+var webpackMiddleware = require("webpack-dev-middleware");
+var webpackHotMiddleware = require("webpack-hot-middleware");
+var webpack = require("webpack");
+var config = require("./webpack.config.js");
+var compiler = webpack(config);
 
- app.use(webpackMiddleware(compiler, {
-  publicPath: config.output.publicPath
-  }));
-  app.use(webpackHotMiddleware(compiler)); // And this line
-
+app.use(
+  webpackMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
+app.use(webpackHotMiddleware(compiler)); // And this line
 
 //
 // if (app.get('env') === 'production') {
 //   app.set('trust proxy', 1) // trust first proxy
 //   sess.cookie.secure = true // serve secure cookies
 // }
-
-
-
-
 
 // flash messages
 // app.use(require('connect-flash')());
@@ -135,10 +133,9 @@ app.use(favicon(path.join(__dirname, 'public/img', 'cs-icon.png')));
 //   next();
 // });
 
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -147,11 +144,11 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
     res.status(500);
-    res.render('404', {
-      title: 'Not found!',
+    res.render("404", {
+      title: "Not found!",
       message: err.message,
       error: err
     });
@@ -161,13 +158,12 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(500)
-  res.render('404', {
-    title: 'Not found!',
+  res.status(500);
+  res.render("404", {
+    title: "Not found!",
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
